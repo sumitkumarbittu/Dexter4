429263dbfe25062da05cf78fb26287dce44f6e77
sumitkumarbittu <144518567+sumitkumarbittu@users.noreply.github.com>
2025-09-29 17:28:50 +0530

    2

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return {"message": "Hello from Render!"}
+# function to generate a random number between 1 and 10
+def rand10():
+    return random.randint(1, 10)
+
+# API endpoint
+@app.route('/api/generaterandom')
+def generate_random():
+    number = rand10()
+    return jsonify(random_number=number)
 
 if __name__ == "__main__":
     # Render provides a PORT env variable
